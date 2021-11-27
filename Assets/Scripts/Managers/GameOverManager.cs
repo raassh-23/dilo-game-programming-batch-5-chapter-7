daf1ddcf759f6140c650f2821e3d6ac40d06e7cb
raassh-23 <muh.nur.abdurrauf@gmail.com>
2021-11-27 21:19:59 +0700

    add project

---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayerHealth playerHealth;       
+    public float restartDelay = 5f;
+    public Text WarningText;      
+
+
+    Animator anim;                          
+    float restartTimer;                    
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playerHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+
+            restartTimer += Time.deltaTime;
+
+            if (restartTimer >= restartDelay)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+
+    public void ShowWarning(float enemyDistance) { 
+        WarningText.text = string.Format("! {0} m", Mathf.RoundToInt(enemyDistance));
+        anim.SetTrigger("Warning");
+    }
+}
